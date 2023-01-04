7cf8e4f97b59e5aa6e18047f96f2dcef89a3aea0
Peter Palmer <58218056+peter-palmer@users.noreply.github.com>
2023-01-04 16:36:28 +0100

    index.js, first version

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const request = require('request');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/proxy', (req, res) => {
+  if (!req.headers.origin && !req.headers['x-requested-with']) {
+    res.status(400).send({error: 'Missing required headers (origin or x-requested-with)'});
+    return;
+  }
+  const options = {
+    url: req.body.url,
+    method: req.body.method,
+    headers: req.body.headers,
+    body: req.body.body
+  }
+  request(options, (error, response, body) => {
+    if (error) {
+      res.send({error});
+    } else {
+      res.send({response, body});
+    }
+  });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(3000, () => {
+  console.log('API listening on port 3000');
+});
